cf011bf6141398bcf163d598116c6a327da57c7d
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-20 15:25:12 -0400

    Update skeleloader.lua

--- skeleloader.lua
+++ skeleloader.lua
@@ -6,6 +6,7 @@ local Players = game:GetService("Players")
 local TS = game:GetService("TweenService")
 local ReSt = game:GetService("ReplicatedStorage")
 local PPS = game:GetService("ProximityPromptService")
+local Wc = game:GetService("Workspace")
 
 local Plr = Players.LocalPlayer
 local Char = Plr.Character or Plr.CharacterAdded:Wait()
@@ -15,3 +16,23 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utiliti
 
 local skelekey = LoadCustomInstance("https://github.com/lucianavfxdude/skelekey_model/blob/main/rawskeleton.rbxm?raw=true")
 skelekey.Parent = Plr.Backpack
+
+local currentrom = ReSt.GameData.LatestRoom.Value
+local roomlist = Wc.CurrentRooms
+local currentrommodel = roomlist[currentrom]
+
+if Char.Key == false and currentrommodel.Lock == false then
+  print("mf equip your skele key, and make sure the room is locked")
+else
+  currentrommodel.Lock.UnlockPrompt.Triggered:Connect(function()
+            	Char:WaitForChild("Humanoid").Health = 0
+				debug.setupvalue(
+					getconnections(game:GetService("ReplicatedStorage").RemotesFolder.DeathHint.OnClientEvent)[1].Function,
+					1,
+					{
+						"mf dont use a spawned in skeleton key",
+						"*gives you -100 revives*"
+					}
+				)
+  end
+end
